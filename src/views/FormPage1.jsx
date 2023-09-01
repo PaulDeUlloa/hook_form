6d1be33c1a3c78794c0ef53d5a56a646dcd9ae9d
Paul De Ulloa <pauld143@gmail.com>
2023-08-31 17:03:18 -0700

    First JSX Form - Base File

---
+++ src/views/FormPage1.jsx
@@ -0,0 +1,16 @@
+
+
+
+const FormPage1 = (props) =>{
+
+
+    return(
+        <>
+            <fieldset>
+                <legend>FormPage1.jsx</legend>
+            </fieldset>
+        </>
+    )
+}
+
+export default FormPage1;
