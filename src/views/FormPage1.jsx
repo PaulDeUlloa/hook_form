795e84f74d2e66817ea95f7ad297f256f9b97ce8
Paul De Ulloa <pauld143@gmail.com>
2023-08-31 22:28:35 -0700

    initial validation commit

--- src/views/FormPage1.jsx
+++ src/views/FormPage1.jsx
@@ -3,7 +3,7 @@ import FormDisplayComponents from "../components/FormDisplayComponents";
 
 
 
-const FormPage1 = (props) =>{
+const FormPage1 = (props) => {
 
     //First Name
     //Last Name
@@ -17,6 +17,8 @@ const FormPage1 = (props) =>{
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
+    const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
+
     // e stands for Event
     //Handler for Form
     const handleSubmit = (e) => {
@@ -24,64 +26,90 @@ const FormPage1 = (props) =>{
         // No database yet
 
         const newData = { firstName, lastName, email, password, confirmPassword };
-        console.log(newData)
+        console.log(newData);
+
     }
 
-    return(
+    return (
         <>
             <fieldset>
                 <legend>FormPage1.jsx</legend>
-                <form onSubmit={ handleSubmit } >   
+                <form onSubmit={handleSubmit} >
                     <div>
                         <label>First Name: </label>
-                        <input 
-                        name="first name" 
-                        type="text" 
-                        value={firstName} 
-                        onChange={(e) => { setFirstName(e.target.value) }}
+                        <input
+                            name="first name"
+                            type="text"
+                            value={firstName}
+                            onChange={(e) => { setFirstName(e.target.value) }}
                         />
+                        {
+                            firstName.length < 2 ?
+                                <p style={{color: "red"}}>First Name must be at least 2 characters</p> :
+                                <></>
+                        }
                     </div>
                     <div>
                         <label>Last Name: </label>
-                        <input 
-                        name="last name" 
-                        type="text" 
-                        value={lastName} 
-                        onChange={(e) => { setLastName(e.target.value) }}
+                        <input
+                            name="last name"
+                            type="text"
+                            value={lastName}
+                            onChange={(e) => { setLastName(e.target.value) }}
                         />
+                        {
+                            lastName.length < 2 ?
+                                <p style={{color: "red"}}>Last Name must be at least 2 characters</p> :
+                                <></>
+                        }
                     </div>
                     <div>
                         <label>Email: </label>
-                        <input 
-                        name="email" 
-                        type="text" 
-                        value={email} 
-                        onChange={(e) => { setEmail(e.target.value) }}
+                        <input
+                            name="email"
+                            type="text"
+                            value={email}
+                            onChange={(e) => { setEmail(e.target.value) }}
                         />
+                        {
+                            email.length < 5 ?
+                                <p style={{color: "red"}}>Email must be at least 5 characters</p> :
+                                <></>
+                        }
                     </div>
                     <div>
                         <label>Password: </label>
-                        <input 
-                        name="password" 
-                        type="password" 
-                        value={password} 
-                        onChange={(e) => { setPassword(e.target.value) }}
+                        <input
+                            name="password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => { setPassword(e.target.value) }}
                         />
+                        {
+                            password.length < 8 ?
+                                <p style={{color: "red"}}>Email must be at least 8 characters</p> :
+                                <></>
+                        }
                     </div>
                     <div>
                         <label>ConfirmPassword: </label>
-                        <input 
-                        name="confirmPassword" 
-                        type="password" 
-                        value={confirmPassword} 
-                        onChange={(e) => { setConfirmPassword(e.target.value) }}
+                        <input
+                            name="confirmPassword"
+                            type="password"
+                            value={confirmPassword}
+                            onChange={(e) => { setConfirmPassword(e.target.value) }}
                         />
+                        {
+                            confirmPassword == password ?
+                            <p style={{color: "red"}}>Confirm Password must be at least 8 characters</p> :
+                            <p style={{color: "red"}}>Passwords must match </p>
+                        }
                     </div>
                     <button>Show Form Info</button>
                 </form>
 
                 <FormDisplayComponents firstName={firstName} lastName={lastName} email={email} password={password} confirmPassword={confirmPassword} />
-                
+
             </fieldset>
         </>
     )
